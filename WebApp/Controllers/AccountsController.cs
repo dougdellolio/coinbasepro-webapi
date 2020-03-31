6907ad370426fda91e85a1a5696541b79bbb65a9
Doug Dellolio <doug.dellolio@gmail.com>
2020-03-30 20:24:15 -0400

    init (#1)

---
+++ WebApp/Controllers/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoinbasePro;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ICoinbaseProClient Client;
+
+        public AccountsController(ICoinbaseProClient client)
+        {
+            Client = client;
+        }
+
+        // GET: api/Accounts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CoinbasePro.Services.Accounts.Models.Account>>> Get()
+        {
+            var allAccounts = await Client.AccountsService.GetAllAccountsAsync();
+
+            return allAccounts.ToArray();
+        }
+    }
+}
